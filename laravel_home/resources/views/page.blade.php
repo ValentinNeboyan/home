3173046630e6c463e14586f887c88fc431fc8add
ValentinNeboyan <valentin_neboyan@ukr.net>
2018-11-02 16:27:59 +0200

    init blog

---
+++ laravel_home/resources/views/page.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentin
+ * Date: 02.11.2018
+ * Time: 15:22
+ */
---
+++ www/practice/pages/1.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentin
+ * Date: 01.11.2018
+ * Time: 12:00
+ */
